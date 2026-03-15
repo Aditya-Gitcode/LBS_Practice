2e269f60d76cbe6a11c5283424edd64ac5bfdceb
Aditya-Gitcode <adityasoreng85@gmail.com>
2026-03-15 09:37:38 +0530

    Commit

---
+++ LBS_Practice/src/Branch1/Program2.java
@@ -0,0 +1,11 @@
+package Branch1;
+//Addition of Two Numbers
+public class Program2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
